d0b1ebbb84ad9e105878a3990d072d2e342e7a2c
nicholastool-tamu <nicholastool@tamu.edu>
2024-10-14 01:27:48 -0500

    Initial Commit of Smart Home Back-End LAMP stack

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php
+//Load sensitive information through .env file
+$env = parse_ini_file('.env');
+
+$servername = $env['DB_HOST'];
+$username = $env['DB_USER'];
+$password = $env['DB_PASSWORD'];
+$dbname = $env['DB_NAME'];
+
+//Create connection to the database using mysqli, allows for OOP
+$connection = new mysqli($servername, $username, $password, $dbname);
+
+//Check connection, show error if connection fails
+if ($conn->connect_error) {
+	die ("Connection failed: " .$conn->connect-error);
+}
+echo "Connected successfully to The Smart Home Database!"; 
+?>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php echo "Hello World! "; ?>
+<?php echo date('Y-m-d H:i:s') ?>
+
